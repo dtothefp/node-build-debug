2b525829769e2294af038adfac647c99dd95deeb
dtothefp <dtothefp@gmail.com>
2014-08-19 22:16:17 -0400

    initial commit

---
+++ bin/build-debug.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+require('shelljs/global');
+
+var cmd = process.argv[2];
+var task    = process.argv[3];
+
+console.log('Runing the ' + cmd + ' ' + task + ' task.'); 
+exec('node --debug-brk $(which ' + cmd + ') ' +  task + ' & node-inspector');
