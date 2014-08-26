31e1e02e0ca6b4c9b723bd0fc44b9b4e499f337b
dtothefp <dtothefp@gmail.com>
2014-08-26 14:49:55 -0400

    update to newer node-debug syntax, bump version

--- bin/build-debug.js
+++ bin/build-debug.js
@@ -6,4 +6,4 @@ var cmd = process.argv[2];
 var task    = process.argv[3];
 
 console.log('Runing the ' + cmd + ' ' + task + ' task.'); 
-exec('node --debug-brk $(which ' + cmd + ') ' +  task + ' & node-inspector');
+exec('node-debug $(which ' + cmd + ') ' +  task);
